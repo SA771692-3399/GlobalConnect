668a18ec6769a57703af22888335dabc362a03f1
kosurusneha <skosuru@albany.edu>
2024-04-28 17:35:48 -0400

    updating blog page

---
+++ source_code/client/src/components/BlogPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Topnav from "./Topnav";
+import Slider from "./Slider";
+import MainNav from "./MainNav";
+import Footer from "./Footer";
+import Blogs from "./Blogs";
+import HeadText from "./HeadText";
+
+import './Topnav.css';
+
+export default function BlogPage() {
+  const data = [
+    {
+      t1: "Our Blogs",
+      t2: "Lets Explore Our Blogs",
+      t3: "As a local business, we take pride in offering a delectable array of traditional Indian food, crafted with authentic recipes passed down through generations. ",
+    },
+  
+  ];
+  return (
+    <>
+      <Topnav />
+      <MainNav />
+      <Slider />
+      <div className="headText">
+      <HeadText t1={data[0].t1} t2={data[0].t2} t3={data[0].t3} />
+      <Blogs />
+      </div>
+      
+    
+      <Footer />
+    </>
+  );
+}
+
+
