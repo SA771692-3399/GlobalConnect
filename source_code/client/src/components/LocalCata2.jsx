18d59af37f8715c37cffffaf4c8b5cea6afb6161
SaideepYamsani <syamsani@albany.edu>
2024-04-28 15:32:19 -0400

    Updated user side products

--- source_code/client/src/components/LocalCata2.jsx
+++ source_code/client/src/components/LocalCata2.jsx
@@ -75,7 +75,116 @@ export default function LocalCata({
                    : "out-of-stock"
                }
              >
-              
+               <div>
+                 <img
+                   src={
+                     product._id
+                       ? `http://localhost:8000/${product.image}`
+                       : product.image
+                   }
+                   alt={product.name}
+                   className="card-img-top"
+                   style={{
+                     objectFit: "cover",
+                     maxWidth: "400px",
+                     maxHeight: "300px",
+                     minWidth: "400px",
+                     minHeight: "300px",
+                     width: "100%",
+                     height: "100%",
+                   }}
+                   onClick={() => handleProductClick(product)}
+                 />
+                 <div className="card-body">
+                   <div style={{ display: "inline-block" }}>
+                     <h5
+                       className="card-text"
+                       style={{ color: "gray", fontSize: "16px" }}
+                     >
+                       {product.name}{" "}
+                       <span
+                         style={{
+                           right: "0",
+                           position: "absolute",
+                           paddingRight: "10px",
+                           fontSize: "12px",
+                           color: "skyblue",
+                         }}
+                       >
+                         {product.category}
+                       </span>
+                     </h5>
+                     <p
+                       style={{
+                         display: "inline",
+                         right: "0",
+                         position: "absolute",
+                         paddingRight: "10px",
+                         marginTop: "-1rem",
+                       }}
+                     >
+                       {product.price}${" "}
+                       <span style={{ color: "#7e5888", fontSize: "12px" }}>
+                         {product.sizeProduct} Size
+                       </span>
+                     </p>
+                     <h5
+                       className="card-text"
+                       style={{ color: "gray", fontSize: "16px" }}
+                     >
+                       <span
+                         style={{
+                           right: "0",
+                           position: "absolute",
+                           paddingRight: "10px",
+                           fontSize: "12px",
+                           marginTop: "10px",
+                         }}
+                       >
+                         {" "}
+                         {product.quantity} in Stock
+                       </span>
+                     </h5>
+                   </div>
+ 
+                   <div
+                     style={{
+                       display: "flex",
+                       justifyContent: "center",
+                       marginTop: "1rem",
+                     }}
+                   >
+                 
+                     <Link to={`/Productwish/${product._id}`}>
+                         <CiHeart size={25} color="#7e5888" />
+                       </Link>
+                   </div>
+                   <br />
+                   <div
+                     style={{
+                       display: "flex",
+                       justifyContent: "center",
+                       marginTop: "-1rem",
+                     }}
+                   >
+                     <button
+                       className="add-to-cart-btn"
+                       onClick={() => addToCart(product._id, product.name)}
+                     >
+                       Add to Cart
+                     </button>
+                     <Link to={`/Productfb/${product._id}`}>
+                       <button
+                         className="add-to-cart-btn ml-3"
+                         onClick={() => addToCart(product._id, product.name)}
+                       >
+                         Raiting
+                       </button>
+                       </Link>
+                   </div>
+                 </div>
+               </div>
+ 
                {!product.quantity || product.quantity === 0 ? (
                  <p className="overlay-text">Out Of Stock</p>
                ) : null}
