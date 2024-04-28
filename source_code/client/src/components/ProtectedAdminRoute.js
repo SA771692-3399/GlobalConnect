2b08f55ed2bcaf7cf0a97c803f79580096c9f27b
sravya <shravyakommula1015@gmail.com>
2024-04-28 11:09:54 -0400

    Updated Admin Routes for Sprint-2

---
+++ source_code/client/src/components/ProtectedAdminRoute.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useAuth } from "../provider/AuthProvider";
+import axios from "axios";
+function ProtectedAdminRoute() {
+  const nav = useNavigate();
+  let { role } = useAuth();
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("No token found");
+  axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+  useEffect(() => {
+    (async () => {
+      try {
+        await axios.get("http://localhost:8000/check-auth");
+      } catch (e) {
+        console.log(e);
+        alert("session expired");
+        nav("/login");
+      }
+    })();
+  }, []);
+
+  if (role === "") {
+    role = localStorage.getItem("Role");
+  }
+
+  if (role !== "Admin") {
+    return <Navigate to="/login"></Navigate>;
+  }
+  return <Outlet />;
+}
+
+export default ProtectedAdminRoute;
+
+
