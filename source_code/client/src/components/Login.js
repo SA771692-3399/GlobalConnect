cab19db0f79fee1a2fc265bc1d67affbdc8f418b
kosurusneha <skosuru@albany.edu>
2024-04-28 10:55:35 -0400

    Updated login for Sprint-2

--- source_code/client/src/components/Login.js
+++ source_code/client/src/components/Login.js
@@ -1,89 +1,189 @@
-import React, { useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import { useAuth } from "../provider/AuthProvider";
-import axios from "axios";
-import logo from "../assets/GlobalConnect.png";
-import "../styles/Login.css";
+import React, { useState } from "react"; 
 
-function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+import { Link, useNavigate } from "react-router-dom"; 
 
-  const { token, setToken } = useAuth();
+import { useAuth } from "../provider/AuthProvider"; 
 
-  const navigate = useNavigate();
+import axios from "axios"; 
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post("http://localhost:8000/login", {
-        UserName: username,
-        Password: password,
-      });
+import logo from "../assets/GlobalConnect.png"; 
 
-      setToken(res.data.token);
+import "../styles/Login.css"; 
 
-      if (res.data.Role === "Admin") {
-        navigate("/admin", { replace: true });
-        window.location.reload();
-      } else if (res.data.Role === "Seller") {
-        navigate("/seller-dashboard", { replace: true });
-        window.location.reload();
-      } else {
-        navigate("/", { replace: true });
-        window.location.reload();
-      }
-    } catch (error) {
-      console.error("Login error:", error);
-    }
-  };
+import { ToastContainer, toast } from "react-toastify"; 
 
-  return (
-    <div className="login_container">
-      <div className="login-box">
-        <img src={logo} alt="Company Logo" className="login-logo" />
-        <h2 className="login-heading">Login</h2>
-        <form className="login-form" onSubmit={handleLogin}>
-          <div className="form-group">
-            <label htmlFor="username" className="form-label">
-              Username:
-            </label>
-            <input
-              type="text"
-              id="username"
-              className="form-input"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password" className="form-label">
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="form-input"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="login_button">
-            Login
-          </button>
-        </form>
-        <div className="additional-options">
-          <p className="register-link">
-            New user? <Link to="/register"> Register</Link>
-          </p>
-          <p className="forgot-password-link">
-            {" "}
-            <Link to="/forget-password">Forget Password</Link>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-}
+import "react-toastify/dist/ReactToastify.css"; 
 
-export default Login;
+ 
+
+function Login() { 
+
+  const [username, setUsername] = useState(""); 
+
+  const [password, setPassword] = useState(""); 
+
+  const { setToken, setRole_ } = useAuth(); 
+
+  const navigate = useNavigate(); 
+
+ 
+
+  const notifySuccess = (message) => toast.success(message); 
+
+  const notifyError = (message) => toast.error(message); 
+
+ 
+
+  const handleLogin = async (e) => { 
+
+    e.preventDefault(); 
+
+    try { 
+
+      const res = await axios.post("http://localhost:8000/login", { 
+
+        UserName: username, 
+
+        Password: password, 
+
+      }); 
+
+      setRole_(res.data.Role); 
+
+      setToken(res.data.token); 
+
+ 
+
+      if (res.data.Role === "Admin") { 
+
+        navigate("/admin"); 
+
+      } else if (res.data.Role === "Seller") { 
+
+        navigate("/seller-dashboard"); 
+
+      } else { 
+
+        navigate("/user"); 
+
+      } 
+
+      notifySuccess("Login successful."); 
+
+    } catch (error) { 
+
+      console.error("Login error:", error); 
+
+      notifyError("Invalid username or password. Please try again."); 
+
+    } 
+
+  }; 
+
+ 
+
+  return ( 
+
+    <div className="login_container"> 
+
+      <ToastContainer /> 
+
+      <div className="login-box"> 
+
+        <img src={logo} alt="Company Logo" className="login-logo" /> 
+
+        <h2 className="login-heading">Login</h2> 
+
+        <form className="login-form" onSubmit={handleLogin}> 
+
+          <div className="form-group"> 
+
+            <label htmlFor="username" className="form-label"> 
+
+              Username: 
+
+            </label> 
+
+            <input 
+
+              type="text" 
+
+              id="username" 
+
+              className="form-input" 
+
+              value={username} 
+
+              onChange={(e) => setUsername(e.target.value)} 
+
+              required 
+
+            /> 
+
+          </div> 
+
+          <div className="form-group"> 
+
+            <label htmlFor="password" className="form-label"> 
+
+              Password: 
+
+            </label> 
+
+            <input 
+
+              type="password" 
+
+              id="password" 
+
+              className="form-input" 
+
+              value={password} 
+
+              onChange={(e) => setPassword(e.target.value)} 
+
+              required 
+
+            /> 
+
+          </div> 
+
+          <button type="submit" className="login_button"> 
+
+            Login 
+
+          </button> 
+
+        </form> 
+
+        <div className="additional-options"> 
+
+          <p className="register-link"> 
+
+            New user? <Link to="/register"> Register</Link> 
+
+          </p> 
+
+          <p className="forgot-password-link"> 
+
+            <Link to="/forget-password">Forget Password</Link> 
+
+          </p> 
+
+        </div> 
+
+      </div> 
+
+    </div> 
+
+  ); 
+
+} 
+
+ 
+
+export default Login; 
+
+ 
+
+ 
