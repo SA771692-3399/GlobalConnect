bdb05ec0c7c93cd74018d6bf7d21755efc8d8b7a
Sphoorthy Akurathi <sakurathi@albany.edu>
2024-04-28 18:00:32 -0400

    add authentication

--- source_code/client/src/provider/AuthProvider.js
+++ source_code/client/src/provider/AuthProvider.js
@@ -1,22 +1,37 @@
 import { useMemo, useContext, useEffect, useState, createContext } from "react";
 
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [token, setTokenPrivate] = useState(localStorage.getItem("token"));
+  const [role, setRole_] = useState("");
   const setToken = (newToken) => {
     setTokenPrivate(newToken);
+  };
+  const setRole = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/check-auth");
+      localStorage.setItem("Role", res.data.role);
+      setRole_(res.data.role);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const removeRole = async () => {
+    localStorage.removeItem("Role");
+    setRole_("");
   };
   useEffect(() => {
     if (token) {
       localStorage.setItem("token", token);
       axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      setRole();
     } else {
       delete axios.defaults.headers.common["Authorization"];
       localStorage.removeItem("token");
+      removeRole();
     }
   }, [token]);
 
@@ -24,6 +39,8 @@ const AuthProvider = ({ children }) => {
     () => ({
       token,
       setToken,
+      role,
+      setRole_,
     }),
     [token]
   );
@@ -31,6 +48,7 @@ const AuthProvider = ({ children }) => {
     <AuthContext.Provider value={contextVal}>{children}</AuthContext.Provider>
   );
 };
+
 export const useAuth = () => useContext(AuthContext);
 
 export default AuthProvider;
