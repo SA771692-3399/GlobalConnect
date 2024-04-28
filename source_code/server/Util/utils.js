def676a410d834a327b59f9b4c01d498dd64bea0
siddarthakurmashetti <skurmashetti@albany.edu>
2024-04-28 12:09:46 -0400

    routing done

---
+++ source_code/server/Util/utils.js
@@ -0,0 +1,34 @@
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = "./uploads/";
+    fs.mkdirSync(uploadDir, { recursive: true });
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { authenticateToken, upload };
